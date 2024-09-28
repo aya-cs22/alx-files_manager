f53a6a5de079c8e1673be2a2c79eb3bce89ef3b0
aya-cs22 <aya786930@gmail.com>
2024-09-28 20:38:20 +0300

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,56 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  // Constructor - Used to create a MongoDB client
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    // Create database URL
+    this.url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(this.url, { useUnifiedTopology: true });
+    this.dbName = database;
+    this.isConnected = false;
+    this.connect();
+  }
+
+  // Asynchronous function to connect to database
+  async connect() {
+    try {
+      await this.client.connect(); // Trying to connect to the database
+      this.isConnected = true;
+    } catch (error) {
+      console.error('Failed to connect to MongoDB', error);
+      this.isConnected = false; // Set to false if connection fails
+    }
+  }
+
+  // Function to check if the database connection is still active
+  isAlive() {
+    return this.isConnected; // Return the connection status
+  }
+
+  // Asynchronous function to get the number of documents in the 'users' collection
+  async nbUsers() {
+    if (!this.isAlive()) {
+      throw new Error('Not connected to the database');
+    }
+    const db = this.client.db(this.dbName);
+    const count = await db.collection('users').countDocuments();
+    return count;
+  }
+
+  // Asynchronous function to get the number of documents in the 'files' collection
+  async nbFiles() {
+    if (!this.isAlive()) {
+      throw new Error('Not connected to the database');
+    }
+    const db = this.client.db(this.dbName);
+    const count = await db.collection('files').countDocuments();
+    return count;
+  }
+}
+
+// Create and export an instance of DBClient
+const dbClient = new DBClient();
+export default dbClient;
