d2a90fe85d44c1b679958b213220855b23a4a8f9
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-29 17:16:08 +0300

    more endpoints to index

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,8 @@ const router = Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get(' /users/me', UserController.getMe);
 
 export default router;
