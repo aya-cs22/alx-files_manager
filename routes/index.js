7c1ca019e2396998260ecab537a7efbc646b5f2c
aya-cs22 <aya786930@gmail.com>
2024-10-01 20:39:40 +0300

    routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
 //  all endpoints of the API
 
-import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
+import { Router } from 'express';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
-
+router.post('/users', UsersController.postNew)
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 export default router;
