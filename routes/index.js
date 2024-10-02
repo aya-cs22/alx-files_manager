02a70ed5b7249b522a277be0ca03f479c4abf113
aya-cs22 <aya786930@gmail.com>
2024-10-02 19:05:28 +0300

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+// import FilesController from '../controllers/FilesController';
 
 const router = Router();
 
@@ -13,7 +13,7 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
+// router.post('/files', FilesController.postUpload);
+// router.get('/files/:id', FilesController.getShow);
+// router.get('/files', FilesController.getIndex);
 export default router;
