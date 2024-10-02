5eece55f4ba52b72ce9bd6d404679c6b61793e5c
hagarSamy <hs1800@fayoum.edu.eg>
2024-10-02 20:49:44 +0300

    back to a previous commit

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,5 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+
 const router = Router();
 
 router.get('/status', AppController.getStatus);
@@ -11,4 +12,5 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+
 export default router;
