bb90e47d1a0fb934330cc1ace0f0d0e860c38e19
aya-cs22 <aya786930@gmail.com>
2024-10-02 17:42:35 +0300

    edit

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew)
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
+router.post('/users', UsersController.postNew);
+// router.post('/files', FilesController.postUpload);
+// router.get('/files/:id', FilesController.getShow);
+// router.get('/files', FilesController.getIndex);
 export default router;
